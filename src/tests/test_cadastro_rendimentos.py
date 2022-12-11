d08150b3b4ee826f20e9dac6de74163d816d1e48
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-12-11 20:26:47 -0300

    Adiciona triangulação em testes de cadastro de rendimentos

--- src/tests/test_cadastro_rendimentos.py
+++ src/tests/test_cadastro_rendimentos.py
@@ -14,3 +14,11 @@ class TestCadastroRendimentos:
         simulador_irpf.cadastra_rendimento("Dividendos", 2000)
 
         assert simulador_irpf.total_rendimentos == 12000
+
+    def test_cadastra_tres_novos_rendimentos(self):
+        simulador_irpf = SimuladorIRPF()
+        simulador_irpf.cadastra_rendimento("Salário", 10000)
+        simulador_irpf.cadastra_rendimento("Dividendos", 2000)
+        simulador_irpf.cadastra_rendimento("Aluguel", 1800)
+
+        assert simulador_irpf.total_rendimentos == 13800
