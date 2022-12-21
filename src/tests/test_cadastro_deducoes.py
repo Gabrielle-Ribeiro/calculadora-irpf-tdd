6a05c6928d56ad09fdebf3bd56359eb433374642
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-12-20 21:00:42 -0300

    Adiciona triangulação em cadastro de deducao
    Co-authored-by: Danilo Domingo <danilow200@gmail.com>

--- src/tests/test_cadastro_deducoes.py
+++ src/tests/test_cadastro_deducoes.py
@@ -1,6 +1,7 @@
 from IRPF.SimuladorIRPF import SimuladorIRPF
 import pytest
 
+
 class TestCadastroDeducoes:
 
     def test_cadastra_uma_nova_deducao(self):
@@ -9,10 +10,17 @@ class TestCadastroDeducoes:
 
         assert simulador_irpf.total_deducoes == 200
 
-
     def test_cadastra_duas_novas_deducoes(self):
         simulador_irpf = SimuladorIRPF()
         simulador_irpf.cadastra_deducao("Previdencia privada", 200)
         simulador_irpf.cadastra_deducao("Funpresp", 150)
 
         assert simulador_irpf.total_deducoes == 350
+
+    def test_cadastra_duas_novas_deducoes(self):
+        simulador_irpf = SimuladorIRPF()
+        simulador_irpf.cadastra_deducao("Previdencia privada", 100)
+        simulador_irpf.cadastra_deducao("Funpresp", 100)
+        simulador_irpf.cadastra_deducao("Saude", 112)
+
+        assert simulador_irpf.total_deducoes == 312
