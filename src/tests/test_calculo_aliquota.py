82e7f8ad7902ec2829f2bc8c02b26de489817ac1
Danilo Domingo <40957754+danilow200@users.noreply.github.com>
2022-12-21 01:07:15 -0300

    Adiciona triangulação na alíquota
    Co-authored-by: Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>

--- src/tests/test_calculo_aliquota.py
+++ src/tests/test_calculo_aliquota.py
@@ -10,9 +10,16 @@ class TestCalculoAliquota:
         assert simulador_irpf.calcula_total_imposto() == 0
         assert simulador_irpf.calcula_total_aliquota() == 0
 
-    def test_calcula_outra_aliquota(self):
+    def test_calcula_segunda_aliquota(self):
         simulador_irpf = SimuladorIRPF()
         simulador_irpf.cadastra_rendimento("Salario", 3000)
 
         assert simulador_irpf.calcula_total_imposto() == pytest.approx(95.20, 0.1)
         assert simulador_irpf.calcula_total_aliquota() == pytest.approx(3.17, 0.1)
+
+    def test_calcula_terceira_aliquota(self):
+        simulador_irpf = SimuladorIRPF()
+        simulador_irpf.cadastra_rendimento("Salario", 10000)
+
+        assert simulador_irpf.calcula_total_imposto() == pytest.approx(1880.64, 0.1)
+        assert simulador_irpf.calcula_total_aliquota() == pytest.approx(18.8, 0.1)
